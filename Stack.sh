d84453c71891a1a0964a1339159b9fb685c21225
Dawid Woźniak <holgrin@gmail.com>
2018-07-26 18:05:28 +0200

    FIX

--- Stack.sh
+++ Stack.sh
@@ -1,24 +1,16 @@
 # Install additional tools for docker and other
-"sudo yum install -y yum-utils device-mapper-persistent-data lvm2 wget",
+sudo yum install -y yum-utils device-mapper-persistent-data lvm2 wget
 # Install docker
-"sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo",
-"sudo yum install -y docker-ce",
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install -y docker-ce
 # Install docker-compose
-"sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose",
-"sudo chmod +x /usr/local/bin/docker-compose",
-# Install git
-"sudo yum install -y git",
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 # Install nodejs
-"curl --silent --location https://rpm.nodesource.com/setup_10.x | sudo bash -",
-"sudo yum install -y nodejs",
+curl --silent --location https://rpm.nodesource.com/setup_10.x | sudo bash -
+sudo yum install -y nodejs
 # Install yarn
-"curl --silent --location https://dl.yarnpkg.com/rpm/yarn.repo | sudo tee /etc/yum.repos.d/yarn.repo",
-"sudo yum install -y yarn",
+curl --silent --location https://dl.yarnpkg.com/rpm/yarn.repo | sudo tee /etc/yum.repos.d/yarn.repo
+sudo yum install -y yarn
 # Start docker
-"sudo systemctl start docker",
-# Create directory for app
-"mkdir /usr/app",
-# Open app directory
-"cd /usr/app",
-# Clone repo to app dir
-"git clone git@gitlab.com:DevDigitalNomad/DashboardTS.git",
+sudo systemctl start docker
