a9453c66faa22cc2af701fb076ded345d8f7f850
Dawid Woźniak <holgrin@gmail.com>
2018-10-10 21:53:54 +0200

    Fix links

--- packages/Api.Node/src/config/index.ts
+++ packages/Api.Node/src/config/index.ts
@@ -1,7 +1,7 @@
 export const spotifyConfig = {
   authenticateURL: "https://accounts.spotify.com/authorize",
   // redirectURI: "http://localhost:4000/spotify/authenticateResult",
-  redirectURI: "https://liquidash.pl/spotify/authenticateResult",
+  redirectURI: "https://api.liquidash.pl/spotify/authenticateResult",
   userScopes: ["user-read-private", "user-read-email"],
   clientID: "5022d32a21ad45a3bfce0835142a3d2c",
   clientSecret: "1c385db25c474181841aabcc6385e457",
@@ -16,7 +16,7 @@ export const digitalOceanConfig = {
 
 export const paypalConfigSandbox = {
   // redirectURI: "http://localhost:4000/paypal/authenticateResult",
-  redirectURI: "https://liquidash.pl/paypal/authenticateResult",
+  redirectURI: "https://api.liquidash.pl/paypal/authenticateResult",
   userScopes: ["openid", "profile", "email", "address", "phone"],
   clientID: "ASfO1H0mADohGRw2pX1mY0f2u4T6LmZzvssekIVfLfJAsqPd-UjN_xEhc7gzj9R6rlDp4HvsSBB8BxK7",
   clientSecret: "EDXICZXmXulpBA_LssZmUr2-3WgoOeA7Qos2dsakEHZXMNXC20Erk6KZ1zpvT0BeIM12E0IJjEnGLkR4",
@@ -27,7 +27,7 @@ export const paypalConfigSandbox = {
 
 export const paypalConfig = {
   // redirectURI: "http://localhost:4000/paypal/authenticateResult",
-  redirectURI: "https://liquidash.pl/paypal/authenticateResult",
+  redirectURI: "https://api.liquidash.pl/paypal/authenticateResult",
   userScopes: ["openid", "profile", "email", "address", "phone"],
   clientID: "AR-cWCrFFOiRP_aSp6VgurEZBCNecPixTWsopyHrBVRiRJ9kde2KgRpIelpy-DtytodJDJMyIC5JPQQ1",
   clientSecret: "EKSZLSU5OoUmabEqr7xATzvPhMWA70Q_XrjWL0ds-ShFNnByv_a0sMKC56avEaTj5sifjgc4vq_QgCUk",
