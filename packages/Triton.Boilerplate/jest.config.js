d47067cd5c56e68058741b208ce234067c179fa8
Dawid Woźniak <holgrin@gmail.com>
2018-11-13 19:25:04 +0100

    simplify boilerplate

--- packages/Triton.Boilerplate/jest.config.js
+++ packages/Triton.Boilerplate/jest.config.js
@@ -1,11 +1,7 @@
 module.exports = {
   clearMocks: true,
-
   coverageDirectory: "coverage",
-
   preset: "ts-jest",
-
   testEnvironment: "node",
-
   testMatch: ["**/__tests__/**/*.ts?(x)", "**/?(*.)+(spec|test).ts?(x)"]
 };
